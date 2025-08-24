ed88c89265dea8d637eb6a3fad3875fc4fe05af3
paulo borges de almeida <borgespaulo72@yahoo.com.br>
2025-08-24 15:49:44 -0300

    feat: Rota GET

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 import express from 'express'
 import cors from 'cors'
+import { prismaClient } from '@prisma/client'
 
 const app = express()
 const PORT = 3000
+const prisma = new prismaClient()
+
 app.use(express.json())
 app.use(cors())
 
 
-app.get('/membros', (req, res) => {
-
+app.get('/membros/:id', async (req, res) => {
+    let membros = []
+    if(req.query) {
+        membros.await.prisma.membros.findMany({
+            where: {
+                name: req.query.name,
+                email: req.query.email,
+                cargo: req.query.cargo
+            }
+        }) 
+    } else {
+        membros.await.prisma.membros.findMany()
+        res.status(200).json(membros)
+    }
 })
 
 app.listen(PORT, () => {
